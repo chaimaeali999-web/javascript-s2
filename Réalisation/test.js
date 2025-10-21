1a17bb9e657b241286cf7e1079b9c744d8f066da
chaimaeali999-web <chaimaeali999@gmail.com>
2025-10-21 10:16:52 +0100

    Delete Réalisation directory

--- Réalisation/test.js
+++
@@ -1,8 +0,0 @@
-let input = document. getElementById("my input");
-let btn = document . getElementById ("bouton");
-let list = document . getElementById("my list");
-
-
-btn.onclick = function (){
-    let text = input.vlue.trim();
-}
